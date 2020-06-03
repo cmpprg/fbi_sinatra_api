17d9941a10f99055b200f769dfe8c4a52ebf5155
Ryan Camp <campryan@comcast.net>
2020-06-03 13:03:14 -0600

    Refactor fbi_service

--- services/fbi_service.rb
+++ services/fbi_service.rb
@@ -1,8 +1,29 @@
 class FBIService
   def crimes_for(state)
-    key = ENV['FBI_KEY']
-    response = Faraday.get("https://api.usa.gov/crime/fbi/sapi/api/estimates/states/#{state}/2010/2020?API_KEY=#{key}")
-    json = JSON.parse(response.body, symbolize_names: true)
-    results = json[:results]
+    hashed_json(state)[:results]
+  end
+
+  private
+
+  def base_url
+    'https://api.usa.gov/crime/fbi/sapi'
+  end
+
+  def connect
+    Faraday.new(base_url) do |req|
+      req.params['API_KEY'] = ENV['FBI_KEY']
+    end
+  end
+
+  def state_response(state)
+    connect.get("api/estimates/states/#{state}/#{this_year - 10}/#{this_year}")
+  end
+
+  def hashed_json(state)
+    JSON.parse(state_response(state).body, symbolize_names: true)
+  end
+
+  def this_year
+    Time.now.year
   end
 end
