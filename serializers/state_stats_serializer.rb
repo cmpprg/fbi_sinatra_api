fd22bb0fc81b1a0a350b66f841d412af0095cb33
Ryan Camp <campryan@comcast.net>
2020-06-03 15:32:23 -0600

    Add list of serializer attributes

--- serializers/state_stats_serializer.rb
+++ serializers/state_stats_serializer.rb
@@ -7,7 +7,17 @@ class StateStatsSerializer
              :homicide_total,
              :rape_total,
              :aggravated_assault_total,
-             :property_crime_total
-
-  set_id nil
+             :property_crime_total,
+             :arson_percent_state,
+             :homicide_percent_state,
+             :rape_percent_state,
+             :aggravated_assault_percent_state,
+             :property_crime_percent_state
+             # :arson_percent_national,
+             # :homicide_percent_national,
+             # :rape_percent_national,
+             # :aggravated_assault_percent_national,
+             # :property_crime_percent_national
+             
+  set_id :state
 end
