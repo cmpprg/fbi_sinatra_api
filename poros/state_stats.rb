3bce1c5fa6eeab07696a92bde66259db894df245
Ryan Camp <campryan@comcast.net>
2020-06-03 15:31:37 -0600

    Add total methods for crimes and percent state methods for crimes

--- poros/state_stats.rb
+++ poros/state_stats.rb
@@ -2,22 +2,59 @@ require 'pry'
 require './poros/state_crimes_by_year'
 
 class StateStats
-  attr_reader :crime_total,
-              :arson_total,
-              :homicide_total,
-              :rape_total,
-              :aggravated_assault_total,
-              :property_crime_total
-  
+  attr_reader :state
+
   def initialize(state)
-    # @crime_total = 
-    # @arson_total = placeholder[:arson]
-    # @homicide_total = placeholder[:homicide]
-    # @rape_total = placeholder[:rape_revised]
-    # @aggravated_assault_total = placeholder[:aggravated_assault]
-    # @property_crime_total = placeholder[:property_crime]
-    @collection = objects(state)
+    @state = state
+    @collection ||= objects(state)
   end
+
+  def arson_total
+    @collection.sum { |crime_for_year| crime_for_year.arson }
+  end
+
+  def homicide_total
+    @collection.sum { |crime_for_year| crime_for_year.homicide }
+  end
+
+  def rape_total
+    @collection.sum { |crime_for_year| crime_for_year.rape }
+  end
+
+  def aggravated_assault_total
+    @collection.sum { |crime_for_year| crime_for_year.aggravated_assault }
+  end
+
+  def property_crime_total
+    @collection.sum { |crime_for_year| crime_for_year.property_crime }
+  end
+
+  def arson_percent_state
+    percent = ((arson_total / total_crime.to_f) * 100)
+    percent.round(1)
+  end
+
+  def homicide_percent_state
+    percent = ((homicide_total / total_crime.to_f) * 100)
+    percent.round(1)
+  end
+
+  def rape_percent_state
+    percent = ((rape_total / total_crime.to_f) * 100)
+    percent.round(1)
+  end
+
+  def aggravated_assault_percent_state
+    percent = ((aggravated_assault_total / total_crime.to_f) * 100)
+    percent.round(1)
+  end
+
+  def property_crime_percent_state
+    percent = ((property_crime_total / total_crime.to_f) * 100)
+    percent.round(1)
+  end
+
+  private
 
   def objects(state)
     last_ten_years(state).map do |result|
@@ -26,7 +63,6 @@ class StateStats
   end
 
   def last_ten_years(state)
-    this_year = Time.now.year
     api_results(state).find_all do |result|
       ((this_year - 10)..this_year).include?(result[:year])
     end
@@ -34,5 +70,13 @@ class StateStats
 
   def api_results(state)
     FBIService.new.crimes_for(state)
+  end
+
+  def this_year
+    Time.now.year
+  end
+
+  def total_crime
+    @collection.sum { |crime_for_year| crime_for_year.total_crime }
   end
 end
