32d7f7e84be4fbb0c7eb414105f12ca9b31a21a4
Ryan Camp <campryan@comcast.net>
2020-06-03 18:27:48 -0600

    Change name of total crime method to grand total crime

--- poros/state_stats.rb
+++ poros/state_stats.rb
@@ -31,27 +31,27 @@ class StateStats
   end
 
   def arson_percent_state
-    percent = ((arson_total / total_crime.to_f) * 100)
+    percent = ((arson_total / grand_total_crime.to_f) * 100)
     percent.round(1)
   end
 
   def homicide_percent_state
-    percent = ((homicide_total / total_crime.to_f) * 100)
+    percent = ((homicide_total / grand_total_crime.to_f) * 100)
     percent.round(1)
   end
 
   def rape_percent_state
-    percent = ((rape_total / total_crime.to_f) * 100)
+    percent = ((rape_total / grand_total_crime.to_f) * 100)
     percent.round(1)
   end
 
   def aggravated_assault_percent_state
-    percent = ((aggravated_assault_total / total_crime.to_f) * 100)
+    percent = ((aggravated_assault_total / grand_total_crime.to_f) * 100)
     percent.round(1)
   end
 
   def property_crime_percent_state
-    percent = ((property_crime_total / total_crime.to_f) * 100)
+    percent = ((property_crime_total / grand_total_crime.to_f) * 100)
     percent.round(1)
   end
 
@@ -103,7 +103,7 @@ class StateStats
     Time.now.year
   end
 
-  def total_crime
+  def grand_total_crime
     @collection.sum { |crime_for_year| crime_for_year.total_crime }
   end
 end
