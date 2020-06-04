f1321f797bcafe6812a98b466ec1970cb1745f47
Ryan Camp <campryan@comcast.net>
2020-06-03 18:27:19 -0600

    Add percentage of totals methods to national stats poros

--- poros/national_stats.rb
+++ poros/national_stats.rb
@@ -1,3 +1,4 @@
+require './services/fbi_service'
 require './poros/national_crime_by_year'
 
 class NationalStats
@@ -25,6 +26,31 @@ class NationalStats
     @collection.sum { |crime_for_year| crime_for_year.property_crime }
   end
 
+  def rape_percent_of_total
+    percent = ((rape_total / grand_total_crime.to_f) * 100)
+    percent.round(1)
+  end
+
+  def property_crime_percent_of_total
+    percent = ((property_crime_total / grand_total_crime.to_f) * 100)
+    percent.round(1)
+  end
+
+  def homicide_percent_of_total
+    percent = ((homicide_total / grand_total_crime.to_f) * 100)
+    percent.round(1)
+  end
+
+  def arson_percent_of_total
+    percent = ((arson_total / grand_total_crime.to_f) * 100)
+    percent.round(1)
+  end
+
+  def aggravated_assault_percent_of_total
+    percent = ((aggravated_assault_total / grand_total_crime.to_f) * 100)
+    percent.round(1)
+  end
+
   private
 
   def objects
@@ -45,5 +71,9 @@ class NationalStats
 
   def this_year
     Time.now.year
+  end
+
+  def grand_total_crime
+    @collection.sum { |crime_for_year| crime_for_year.total_crime }
   end
 end
